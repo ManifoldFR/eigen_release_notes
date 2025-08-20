c3488f6fdb142f1c34b2f069ec7382840974ab64
ManifoldFR <wilson.jallet@polytechnique.org>
2025-08-20 20:45:19 +0200

    pull_milestone_mrs.py : add filenames to diff
    pull_milestone_mrs.py : keep paths in changes

--- pull_milestone_mrs.py
+++ pull_milestone_mrs.py
@@ -1,6 +1,8 @@
 import gitlab
 import os
 import orjson
+import re
+from rich.progress import track
 from dotenv import load_dotenv
 from gitlab.v4.objects.merge_requests import ProjectMergeRequest
 
@@ -9,6 +11,7 @@ load_dotenv()
 gl = gitlab.Gitlab(private_token=os.environ.get("GITLAB_API_KEY"))
 
 EIGEN_PROJECT_ID = 15462818
+comment_pattern = re.compile(r"<!--.*?-->", re.DOTALL)
 project = gl.projects.get(EIGEN_PROJECT_ID)
 
 project.pprint()
@@ -31,20 +34,31 @@ field_names = [
     "labels",
     "state",
 ]
+author_keep_keys = {"id", "username", "name"}
+
+
+def filter_dict(d: dict, keys_keep: set):
+    keys = set(d.keys())
+    keys_to_del = keys.difference(keys_keep)
+    for key in keys_to_del:
+        del d[key]
+
 
 mr_filtered_entries = []
 i = 0
 
-for mr in merge_requests:
+for mr in track(merge_requests):
     mr: ProjectMergeRequest
     # skip open or closed MRs
     if mr.state != "merged":
         continue
     changes = mr.changes()
-    print(f"[{i: 4d}] MR #{mr.iid} - '{mr.title}' (merged at {mr.merged_at})")
-    data = {key: changes[key] for key in field_names}
-    data["changes"] = [m["diff"] for m in data["changes"]]
-    mr_filtered_entries.append(data)
+    print(f"MR #{mr.iid} - '{mr.title}' (merged at {mr.merged_at})")
+    item = {key: changes[key] for key in field_names}
+    filter_dict(item["author"], author_keep_keys)
+    # strip multiline comment
+    item["description"] = comment_pattern.sub("", item["description"]).strip()
+    mr_filtered_entries.append(item)
     i += 1
     # if i == 40:
     #     break
